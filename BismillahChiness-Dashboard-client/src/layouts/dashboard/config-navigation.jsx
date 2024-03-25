fd95ae15e63abb65025e93d8958e301bcb97efbd
RF-Rifat <rfrifat6344@gmail.com>
2024-03-25 22:47:46 +0600

    product navigation change to food

--- BismillahChiness-Dashboard-client/src/layouts/dashboard/config-navigation.jsx
+++ BismillahChiness-Dashboard-client/src/layouts/dashboard/config-navigation.jsx
@@ -17,11 +17,6 @@ const navConfig = [
     icon: icon('ic_user'),
   },
   {
-    title: 'product',
-    path: '/products',
-    icon: icon('ic_cart'),
-  },
-  {
     title: 'blog',
     path: '/blog',
     icon: icon('ic_blog'),
