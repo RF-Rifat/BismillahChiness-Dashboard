4133bf952557dbe9816998dd4c906cbf7b9aeaad
RF-Rifat <rfrifat6344@gmail.com>
2024-03-25 19:07:36 +0600

    dashborad re designed

--- BismillahChiness-Dashboard-client/src/main.jsx
+++ BismillahChiness-Dashboard-client/src/main.jsx
@@ -1,35 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes/Routes.jsx";
+import App from './app';
 
-import { Toaster } from "react-hot-toast";
-import { MaterialTailwindControllerProvider } from "./Context/index.jsx";
-import UserProvider from "./Authentication/AuthProvider.jsx";
-import AdminProvider from "./Context/AdminProvider.jsx";
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <MaterialTailwindControllerProvider>
-          <Toaster />
-          <UserProvider>
-            <AdminProvider>
-              <RouterProvider router={router} />
-            </AdminProvider>
-          </UserProvider>
-        </MaterialTailwindControllerProvider>
-      </QueryClientProvider>
-    </Provider>
-  </React.StrictMode>
+root.render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <Suspense>
+        <App />
+      </Suspense>
+    </BrowserRouter>
+  </HelmetProvider>
 );
