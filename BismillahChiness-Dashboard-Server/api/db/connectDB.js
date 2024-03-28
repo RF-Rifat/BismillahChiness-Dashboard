c30c4d4ea0b986c96c9b84f161850082d5bafa66
RF-Rifat <rfrifat6344@gmail.com>
2024-03-28 23:28:42 +0600

    server base url setup complete

--- BismillahChiness-Dashboard-Server/api/db/connectDB.js
+++ BismillahChiness-Dashboard-Server/api/db/connectDB.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 // Function to generate MongoDB connection URI
 const getConnectionString = () => {
+  console.log(process.env.DB_PASS);
   let connectionUrl;
   connectionUrl = process.env.MONGO_URI;
   connectionUrl = connectionUrl.replace("<password>", process.env.DB_PASS);
