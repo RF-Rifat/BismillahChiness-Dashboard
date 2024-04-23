fb3bd762eb45a02b0a9b08b866ca1b5f78159270
RF-Rifat <rfrifat6344@gmail.com>
2024-04-23 12:20:40 +0600

    order schema declared

---
+++ BismillahChiness-Dashboard-Server/api/models/order.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => /\S+@\S+\.\S+/.test(value),
+      message: "Please enter a valid email address.",
+    },
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+
+const cartItemSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  imageSrc: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
+
+const orderSchema = new mongoose.Schema({
+  user: userSchema,
+  cartData: [cartItemSchema],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
