25f721bf7b7ba7029efe6310299d2f97844c2331
RF-Rifat <rfrifat6344@gmail.com>
2024-04-23 23:06:27 +0600

    error fixed

--- BismillahChiness-Dashboard-Server/api/models/order.js
+++ BismillahChiness-Dashboard-Server/api/models/order.js
@@ -65,7 +65,7 @@ const orderSchema = new mongoose.Schema(
           return value.length > 0;
         },
         message: "Cart data cannot be empty.",
-      },w
+      },
     },
     totalPrice: {
       type: Number,
